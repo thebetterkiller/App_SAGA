24cd1565fe9b0c8f834c378ea74e7069aa0c2356
a-santos <afonsosantos@gmail.com>
2018-03-04 23:40:49 +0000

    New class Professor

---
+++ src/Main.java
@@ -0,0 +1,17 @@
+package com.saga.app;
+
+public class Main {
+
+    public Main() {
+        System.out.println("#################### APP - Versão LINHA COMANDOS #################### ");
+        System.out.println("Sou o MAIN");
+
+    }
+
+    public static void main(String[] args) {
+        new Main();
+
+    }
+
+
+}
---
+++ src/com/saga/app/Professor.java
@@ -0,0 +1,14 @@
+package com.saga.app;
+
+import java.util.LinkedList;
+
+public class Professor {
+
+    private String nome;
+    private long numero;
+
+    public Professor(String nome, long numero) {
+        this.nome = nome;
+        this.numero = numero;
+    }
+}
